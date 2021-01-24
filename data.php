5b0b0206f1eeac7dec5b54bcda53a04ad47288b9
Osama Aburabie <aburabie.osama@gmail.com>
2021-01-24 09:18:06 +0200

    done

--- data.php
+++ data.php
@@ -4,8 +4,7 @@
                                       'id' => '1',
                                       'name' => 'Ashraf ',
                                       'birthday' => '1996',
-                                      'github account' => 'https://github.com/ashraf-aljabari',
-                                      'linkedin' => 'https://www.linkedin.com/in/ashraf-aljabari/',
+                                      
                                       'image' => 'https://avatars0.githubusercontent.com/u/71584075?s=460&u=1f2699d33de5e5c2299c21efa4a7b807e5341923&v=4',
                                       'projects' => array(
 
@@ -18,8 +17,7 @@
                                       'id' => '2',
                                       'name' => 'Monther ',
                                       'birthday' => '1996',
-                                      'github account' => 'https://github.com/Monther-Twaissi',
-                                      'linkedin' => 'https://www.linkedin.com/in/monther-twaissi/',
+                                      
                                       'image' => 'https://avatars2.githubusercontent.com/u/71584000?s=460&u=4ee019affa6883c074003bb0bd7ddba2ce8e1021&v=4',
                                       'projects' => array(
 
@@ -32,8 +30,7 @@
                                       'id' => '3',
                                       'name' => 'Mohammad Bdour',
                                       'birthday' => '1997',
-                                      'github account' => 'https://github.com/mhmdbdour',
-                                      'linkedin' => 'https://www.linkedin.com/in/ashraf-aljabari/',
+                                      
                                       'image' => 'https://avatars0.githubusercontent.com/u/72431203?s=460&u=232c95d84afc3c7419d9ca6cdb5ded6cba6f541f&v=4',
                                       'projects' => array(
 
@@ -46,8 +43,7 @@
                                       'id' => '4',
                                       'name' => 'Ahmad',
                                       'birthday' => '1996',
-                                      'github account' => 'https://github.com/ahmada3mar',
-                                      'linkedin' => 'https://www.linkedin.com/in/ahmada3mar/',
+                                      
                                       'image' => 'https://avatars0.githubusercontent.com/u/71763607?s=400&u=40b3c151bf4dbd5216fcc21363ffb7533d1beccb&v=4',
                                       'projects' => array(
 
@@ -60,9 +56,8 @@
                                       'id' => '5',
                                       'name' => 'Abdullah',
                                       'birthday' => '2002',
-                                      'github account' => 'https://github.com/Abdallah-Alhajeid',
-                                      'linkedin' => 'https://www.linkedin.com/in/abdallah-alhajeid/',
-                                      'image' => 'https://avatars.githubusercontent.com/u/39887091?s=400&v=4',
+                                      
+                                      'image' => 'https://media-exp1.licdn.com/dms/image/C4D35AQF4BZJOyB9BRw/profile-framedphoto-shrink_800_800/0/1610733687709?e=1611561600&v=beta&t=uBoUZUu_21MMLQMG6GEwnwDn9C1Kg_Vozha1MvVG6_s',
                                       'projects' => array(
 
                                                            array('project_name' => 'instagram clone', 'is_compleated' => 'yes'),
@@ -74,8 +69,7 @@
                                       'id' => '6',
                                       'name' => 'Osama',
                                       'birthday' => '1999',
-                                      'github account' => 'https://github.com/OsamaAburabie',
-                                      'linkedin' => 'https://www.linkedin.com/in/osama-aburabi-e-1abb791b6/',
+                                     
                                       'image' => 'https://avatars.githubusercontent.com/u/67992414?s=400&u=c503c163561fb011dcfeac3d7b475735582b35c8&v=4',
                                       'projects' => array(
                                                            array('project_name' => 'instagram clone', 'is_compleated' => 'yes'),
@@ -87,8 +81,7 @@
                                       'id' => '7',
                                       'name' => 'Abdel rahman',
                                       'birthday' => '2000',
-                                      'github account' => 'https://github.com/abdAbdaldeen',
-                                      'linkedin' => 'https://www.linkedin.com/in/abd-abdaldeen/',
+                                      
                                       'image' => 'https://avatars.githubusercontent.com/u/71769628?s=400&u=7a6b1a6a0e0dddba899a48adc799c3e63431dd9b&v=4',
                                       'projects' => array(
 
